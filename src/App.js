a6640e418d13d16fcb26c62db37970e332129095
Konul Aliyeva <konulali21@gmail.com>
2022-09-15 20:56:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useRef } from "react";
 
-function App() {
+function debounce(callback, ms = 500) {
+  let timerId = null;
+  return function () {
+    if (timerId) {
+      clearTimeout(timerId);
+    }
+    timerId = setTimeout(callback, ms);
+  };
+}
+function App(props) {
+  const [result, setResult] = useState([]);
+  const inputRef = useRef(null);
+
+  const requestFunc = async () => {
+    try {
+      const data = await fetch(
+        `https://api.thecatapi.com/v1/images/search?breed_ids=${inputRef.current.value}`
+      );
+
+      let response = await data.json();
+
+      setResult(response);
+    } catch (err) {
+      console.log(err);
+    }
+
+  };
+  const handleChangeInput = () => {   
+
+    debounce (function () {
+   console.log('value' + inputRef.current.value)
+     requestFunc(test );
+    })}
+
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={handleChangeInput} ref={inputRef} />
+      {result.map((item) => {
+        return (
+          <div key={item.id}>
+            <img src={item.url} widt="400" height="400" />
+          </div>
+        );
+      })}
     </div>
   );
 }
+
+// Log to console
 
 export default App;
